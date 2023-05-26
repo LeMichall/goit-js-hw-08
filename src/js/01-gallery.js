fcb82fa3377d43b487664ba1ddfafc905ad1d637
LeMichall <120519238+LeMichall@users.noreply.github.com>
2023-05-26 20:36:10 +0200

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,47 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const createGallery = () => {
+  //"virtual" array
+  let tempGallery = [];
+  //creating each element
+  galleryItems.forEach((e, i) => {
+    const { preview, original, description } = galleryItems[i];
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    const img = document.createElement('img');
+    // assign classes
+    a.classList.add('gallery__item');
+    img.classList.add('gallery__image');
+    // assign attributes
+    a.setAttribute('href', original);
+    img.setAttribute('src', preview);
+    img.setAttribute('alt', description);
+    //creating proper item
+    li.appendChild(a).appendChild(img);
+    tempGallery.push(li);
+  });
+  //push to real gallery
+  gallery.append(...tempGallery);
+};
+createGallery();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'outside',
+  captionDelay: 250,
+});
+
+function eventHandler(event) {
+  // blocking unwanted click behaviour
+  event.preventDefault();
+  if (event.target.nodeName !== 'IMG') return;
+
+  lightbox.open(event.target);
+}
+
+gallery.addEventListener('click', eventHandler);
