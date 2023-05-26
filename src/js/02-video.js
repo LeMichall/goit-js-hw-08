3e604251fc23cd247409c0760b6731ea4beadd45
LeMichall <120519238+LeMichall@users.noreply.github.com>
2023-05-26 21:24:06 +0200

    added packages

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('play', function () {
+  console.log('played the video');
+});
+player.on('timeupdate', () => {
+  console.log('update');
+});
+player.on('pause', () => {
+  console.log('paused the video');
+});
