a827c4307ea0565e89f6e9b37a12ceaa8f675fe2
LeMichall <120519238+LeMichall@users.noreply.github.com>
2023-05-27 00:38:40 +0200

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,13 +1,15 @@
 import Player from '@vimeo/player';
+var throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('play', function () {
-  console.log('played the video');
-});
-player.on('timeupdate', () => {
-  console.log('update');
-});
-player.on('pause', () => {
-  console.log('paused the video');
-});
+function updateTimer(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+function setTimer() {
+  player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+}
+player.on('timeupdate', throttle(updateTimer, 1000));
+
+iframe.addEventListener('load', setTimer);
