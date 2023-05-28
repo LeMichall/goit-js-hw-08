b6dafd75799f91dce3bf553108853d1ad10af521
LeMichall <120519238+LeMichall@users.noreply.github.com>
2023-05-28 13:26:41 +0200

    task 3 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -2,14 +2,14 @@ import Player from '@vimeo/player';
 var throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
+// updating local storage with current timer
 function updateTimer(data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
 }
-
+// setting video timer from localstorage
 function setTimer() {
   player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
 }
-player.on('timeupdate', throttle(updateTimer, 1000));
 
+player.on('timeupdate', throttle(updateTimer, 1000));
 iframe.addEventListener('load', setTimer);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,31 @@
-
+var throttle = require('lodash.throttle');
+const inputEmail = document.querySelector(`input[name="email"]`);
+const inputMessage = document.querySelector(`textarea[name="message"]`);
+const form = document.querySelector(`.feedback-form`);
+const inputData = {
+  email: '',
+  message: '',
+};
+// saving values in local storage
+function localSave() {
+  inputData.email = inputEmail.value;
+  inputData.message = inputMessage.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(inputData));
+}
+// loading values from local storage
+function localLoad() {
+  const savedData =
+    JSON.parse(localStorage.getItem('feedback-form-state')) || '';
+  inputEmail.value = savedData.email || '';
+  inputMessage.value = savedData.message || '';
+}
+// clearing inputs and local storage
+function localSubmit(event) {
+  event.preventDefault();
+  console.log(inputData);
+  localStorage.removeItem('feedback-form-state');
+  form.reset();
+}
+window.addEventListener('load', localLoad);
+form.addEventListener('submit', localSubmit);
+form.addEventListener('input', throttle(localSave, 500));
